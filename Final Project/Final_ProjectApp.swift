f9005a3da57120bdb39b61339097723d68699c8c
Nicole Shimamoto <nicshimamoto@gmail.com>
2023-06-27 14:34:18 -0700

    Initial Commit

---
+++ Final Project/Final_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Final_ProjectApp.swift
+//  Final Project
+//
+//  Created by Nicole Shimamoto on 6/27/23.
+//
+
+import SwiftUI
+
+@main
+struct Final_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
