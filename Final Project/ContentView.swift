3366718f68bf623d5d4258550c6ab209dc066908
Nicole Shimamoto <nicshimamoto@gmail.com>
2023-06-29 14:58:55 -0700

    more titles

--- Final Project/ContentView.swift
+++ Final Project/ContentView.swift
@@ -16,21 +16,29 @@ struct ContentView: View {
                 Color(red: 255.0, green: 237.0, blue: 225.0)
                 VStack(spacing: 25.0) {
                     HStack() {
+                        VStack {
+                            Text("Hi")
+                                .font(.largeTitle)
+                                .fontWeight(.bold)
+                                .foregroundColor(Color(hue: 0.653, saturation: 0.889, brightness: 0.366))
+                                .multilineTextAlignment(.leading)
+                            Text("         There!")
+                                .font(.largeTitle)
+                                .fontWeight(.bold)
+                                .foregroundColor(Color(hue: 0.653, saturation: 0.889, brightness: 0.366))
+                        }
                         Image("leaf")
                             .resizable()
                             .aspectRatio(contentMode: .fit)
                             .frame(width: 130)
-                
-                        Text("Grow from your glow")
-                            .font(.headline)
-                            .multilineTextAlignment(.center)
                         
                     }
-                    Text("Hi there!")
-                        .font(.largeTitle)
-                        .fontWeight(.bold)
-                        .foregroundColor(Color(hue: 0.653, saturation: 0.889, brightness: 0.366))
+                    Text("Grow from your glow")
+                        .font(.headline)
+                        .foregroundColor(Color(hue: 0.561, saturation: 0.433, brightness: 0.931))
+                        .multilineTextAlignment(.center)
                     Text("What is your name?")
+                        .font(.title2)
                         .foregroundColor(Color(hue: 0.653, saturation: 0.889, brightness: 0.366))
                     TextField("", text: $name)
                         .multilineTextAlignment(.center)
