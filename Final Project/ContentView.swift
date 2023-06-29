04459aeafefdabc6c169b4247af290d1ac41cd07
Nicole Shimamoto <nicshimamoto@gmail.com>
2023-06-29 14:25:07 -0700

    Titles

--- Final Project/ContentView.swift
+++ Final Project/ContentView.swift
@@ -12,20 +12,39 @@ struct ContentView: View {
     @State private var textTitle = "What is your name?"
     var body: some View {
         NavigationStack {
-            VStack(spacing: 30.0) {
-                Image("leaf")
-                Text("Hi there!")
-                    .font(.largeTitle)
-                    .fontWeight(.bold)
-                    .foregroundColor(Color(hue: 0.653, saturation: 0.889, brightness: 0.366))
-                    
-                Text("What is your name?")
-                TextField("", text: $name)
-                    .multilineTextAlignment(.center)
-                    .border(Color.blue, width : 0.5)
-                    .cornerRadius(2.5)
-                    .padding()
-        }
+            ZStack {
+                Color(red: 255.0, green: 237.0, blue: 225.0)
+                VStack(spacing: 25.0) {
+                    HStack() {
+                        Image("leaf")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 130)
+                
+                        Text("Grow from your glow")
+                            .font(.headline)
+                            .multilineTextAlignment(.center)
+                        
+                    }
+                    Text("Hi there!")
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                        .foregroundColor(Color(hue: 0.653, saturation: 0.889, brightness: 0.366))
+                    Text("What is your name?")
+                        .foregroundColor(Color(hue: 0.653, saturation: 0.889, brightness: 0.366))
+                    TextField("", text: $name)
+                        .multilineTextAlignment(.center)
+                        .border(Color.blue, width : 0.5)
+                        .cornerRadius(2.5)
+                        .padding()
+                    Button("Submit Name"){
+                    textTitle = "Welcome, \(name)!"
+                    }
+                    .buttonStyle(.bordered)
+            }
+            }
+            .navigationTitle("Luminosity")
+            .navigationBarTitleDisplayMode(.inline)
             
             
         }
