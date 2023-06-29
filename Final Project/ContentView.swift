35b9be2565af79001ce72b3fd541cbf7d04828f5
Nicole Shimamoto <nicshimamoto@gmail.com>
2023-06-29 10:26:49 -0700

    test new change

--- Final Project/ContentView.swift
+++ Final Project/ContentView.swift
@@ -13,7 +13,8 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Hello, world!!")
+            Text("This is a new change")
         }
         .padding()
     }
