cbaed6625274b7a00a0ebf4b4c1988d454f99648
Emi Uzun <uzunestar2004@gmail.com>
2023-06-30 15:15:31 -0500

    finished top part of q2

--- Final Project/q2.swift
+++ Final Project/q2.swift
@@ -2,9 +2,42 @@ import SwiftUI
 struct q2: View {
     var body: some View {
         NavigationStack{
-            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            VStack() {
+                Text("Question 2 / 5")
+                    .offset(y: -250)
+                    .padding()
+                Text("What are your main skin concerns?")
+                    .font(.title)
+                    .multilineTextAlignment(.center)
+                    .offset(y: -240)
+                    .padding()
+                
+                NavigationLink(destination: q2()) {
+                    Text("Dry")
+                        .multilineTextAlignment(.center)
+                            .buttonStyle(.bordered)
+                     
+                        
+                }
+                .frame(height : 10)
+                NavigationLink(destination: q2()) {
+                    Text("Normal")
+                }
+                NavigationLink(destination: q2()) {
+                    Text("Oily")
+                }
+                NavigationLink(destination: q2()) {
+                    Text("Combination")
+                }
+                NavigationLink(destination: q2()) {
+                    Text("Sensitive")
+                }
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity) // Expand the VStack to fill the whole view
+            .background(Color("Color1").ignoresSafeArea()) // Set the background color and ignore safe area insets
         }
     }
+    
 }
 struct q2_Previews: PreviewProvider {
     static var previews: some View {
---
+++ Final Project/q3.swift
@@ -0,0 +1,20 @@
+//
+//  q3.swift
+//  Final Project
+//
+//  Created by Emi Uzner on 6/30/23.
+//
+
+import SwiftUI
+
+struct q3: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct q3_Previews: PreviewProvider {
+    static var previews: some View {
+        q3()
+    }
+}
