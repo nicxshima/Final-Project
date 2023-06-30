963d5861ad9ca13c3dc7fe447e13c4dd02f36140
Nicole Shimamoto <nicshimamoto@gmail.com>
2023-06-29 20:11:28 -0700

    new file

---
+++ Final Project/reminder_nav.swift
@@ -0,0 +1,20 @@
+//
+//  reminder_nav.swift
+//  Final Project
+//
+//  Created by Nicole Shimamoto on 6/29/23.
+//
+
+import SwiftUI
+
+struct reminder_nav: View {
+    var body: some View {
+        Text("The weather today is")
+    }
+}
+
+struct reminder_nav_Previews: PreviewProvider {
+    static var previews: some View {
+        reminder_nav()
+    }
+}
