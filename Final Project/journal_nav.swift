1369d60bdd89f15a2c3655b7e55b9c4fd14692f9
Nhi Tran <contactinfo.nhi@gmail.com>
2023-06-29 20:58:43 -0700

    created journal_nav

---
+++ Final Project/journal_nav.swift
@@ -0,0 +1,20 @@
+//
+//  journal_nav.swift
+//  Final Project
+//
+//  Created by Nhi Tran on 6/29/23.
+//
+
+import SwiftUI
+
+struct journal_nav: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct journal_nav_Previews: PreviewProvider {
+    static var previews: some View {
+        journal_nav()
+    }
+}
