68f321837d2aef04e2201a3d6c6309db7be8049b
Nhi Tran <contactinfo.nhi@gmail.com>
2023-06-30 03:01:28 -0700

    created product_nav

---
+++ Final Project/product_nav.swift
@@ -0,0 +1,20 @@
+//
+//  product_nav.swift
+//  Final Project
+//
+//  Created by Nhi Tran on 6/30/23.
+//
+
+import SwiftUI
+
+struct product_nav: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct product_nav_Previews: PreviewProvider {
+    static var previews: some View {
+        product_nav()
+    }
+}
