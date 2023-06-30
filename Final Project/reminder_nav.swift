8992d6889d652a24f1ace22e922f848c154b2843
Nicole Shimamoto <nicshimamoto@gmail.com>
2023-06-30 01:01:01 -0700

    reminders

--- Final Project/reminder_nav.swift
+++ Final Project/reminder_nav.swift
@@ -8,20 +8,72 @@
 import SwiftUI
 
 struct reminder_nav: View {
-    @State private var buttonText = "Hi"
+    @State private var showInfo = false
+    @State private var showInfo1 = false
+    @State private var showInfo2 = false
+    @State private var water = ""
+    @State private var textTitle = "Daily water tracker"
     var body: some View {
         ZStack {
             Color("Color3").edgesIgnoringSafeArea(.all)
-            VStack {
+            VStack(spacing: 15.0) {
                 Text("The weather today is")
                     .font(.title2)
-                HStack {
-                    Text(buttonText)
-                    Button(action: {
-                        buttonText = "Hi"}) {
-                            Text("bye")
-                        }
+                HStack(spacing: 15.0) {
+                    Image("sun")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 80)
+                    Image("cloudy")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 80)
+                    Image("rainy")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 80)
                     
+                }
+                Button("sunny") {
+                    showInfo.toggle()
+                }
+                
+                if showInfo {
+                    
+                    Text("Today is a sunny day, make sure to wear lots of sunscreen and reapply when needed!")
+                }
+                
+                
+                Button("cloudy") {
+                    showInfo1.toggle()
+                }
+                
+                if showInfo1 {
+                    Text("Looks like it will be a cloudy day today, make sure to still apply sunscreen!")
+                }
+                Button("rainy") {
+                    showInfo2.toggle()
+                }
+                
+                if showInfo2 {
+                    Text("Rainy day today, don't forget to put on sunscreen! UV rays can still penetrate skin on rainy or cloudy days.")
+                }
+                
+                VStack {
+                    Text("Daily water tracker")
+                        .font(.title2)
+                    TextField("", text: $water)
+                        .multilineTextAlignment(.center)
+                        .font(.title)
+                        .border(Color.black, width : 1)
+                        .padding()
+                    
+                    Button("Enter") {
+                        textTitle = "You drank \(water) cups of water!"
+        
+                    }
+                }
+                
                 }
             }
         }
@@ -36,4 +88,4 @@ struct reminder_nav: View {
             reminder_nav()
         }
     }
-}
+
